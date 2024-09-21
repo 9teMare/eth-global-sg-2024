bf99a626e63fe4b6f88911152ecb5b6e9af6dba9
ddx-510 <ddx0510@gmail.com>
2024-09-22 04:29:50 +0800

    feat: add neon card

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: "udhrubteotgugzsxqbgf.supabase.co",
+                protocol: "https",
+            },
+        ],
+    },
+};
 
 export default nextConfig;
